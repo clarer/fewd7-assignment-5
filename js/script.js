38bdeff7a694d877270dffa5a922ec01914c14f3
clarer26 <clare@nativetechnolgy.com.au>
2015-09-16 20:13:29 +1000

    complete!

--- js/script.js
+++ js/script.js
@@ -1,47 +1,41 @@
 
 /*
 THE BRIEF:
-When a user enters acceptable text into #city-type id
-then hits enter or clicks #submit-btn id
-change the body background to show the image matching the acceptable text
+When a user enters acceptable text into '#city-type' id,
+then hits enter or clicks '#submit-btn' id,
+change the 'body background' to show the image matching the acceptable text.
 */
 
 
 $(document).ready(function() {
 
-
     var images = ["url(images/nyc.jpg)", "url(images/sf.jpg)", "url(images/la.jpg)", "url(images/austin.jpg)", "url(images/sydney.jpg)"];
-    // these is my array of images that will appear in the background when the user types the acceptable text.
+    // this is my array of images that will appear in the background when the user types the acceptable text.
 
+    function changeImages(event) {
+    // this is the function called in the final click function.
+        
+        event.preventDefault();
+        // this prevents refresh of the page.
+        var cityInputVal = $("#city-type").val().toLowerCase();
+        // this variable allows the user enter either all lowercase or case sensitive text.
 
-        // input.toLowerCase();
-        // now I need to figure out how to use the above thing
-        // so I can use it in each of the following 'if' statements.
-        // var newStuff = $("#city-type").val().toLowerCase();
-
-
-    function changeImages() {
-    // this is my function that is used below, that maps back to the image in my 
-        if (cityInputVal === "New York" || cityInputVal === "New York City" || cityInputVal === "NYC") {
+        if (cityInputVal === "new york" || cityInputVal === "new york city" || cityInputVal === "nyc") {
             $("body").css("background", images[0]);
-        } else if (cityInputVal === "San Francisco" || cityInputVal === "SF" || cityInputVal === "Bay Area" ) {
+        } else if (cityInputVal === "san francisco" || cityInputVal === "sf" || cityInputVal === "bay area" ) {
             $("body").css("background", images[1]);
-        } else if (cityInputVal === "Los Angeles" || cityInputVal === "LA" || cityInputVal === "LAX") {
+        } else if (cityInputVal === "los angeles" || cityInputVal === "la" || cityInputVal === "lax") {
             $("body").css("background", images[2]);
-        } else if (cityInputVal === "Austin" || cityInputVal === "ATX") {
+        } else if (cityInputVal === "austin" || cityInputVal === "atx") {
             $("body").css("background", images[3]);
-        } else if (cityInputVal === "Sydney" || cityInputVal === "SYD") {
+        } else if (cityInputVal === "sydney" || cityInputVal === "syd") {
             $("body").css("background", images[4]);
         }
+        // this is the list of acceptable text that maps to the correct images from my 'images' array.
+
     }
 
-
-    $("#submit-btn").click(function(event) {
-    // this is the function that runs when the user enters acceptable text and presses 'enter' or clicks 'submit'.
-        cityInputVal = $("#city-type").val();
-        changeImages();
-        event.preventDefault();
-    });
-
+    $("#submit-btn").click(changeImages);
+    // this runs when the user enters acceptable text and presses 'enter' or clicks 'submit'.
 
 });
