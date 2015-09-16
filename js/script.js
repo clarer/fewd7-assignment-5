65d83131479dbb90c236c0dd6aedc24e9e912f9b
clarer26 <clare@nativetechnolgy.com.au>
2015-09-16 18:37:24 +1000

    it's alive!!!

--- js/script.js
+++ js/script.js
@@ -1,60 +1,47 @@
 
 /*
-the brief:
-when a user enters text into #city-type
-store and search for similar tags
-change the background to show the image matching those tags
+THE BRIEF:
+When a user enters acceptable text into #city-type id
+then hits enter or clicks #submit-btn id
+change the body background to show the image matching the acceptable text
 */
 
 
 $(document).ready(function() {
 
 
-    var images = ["../images/nyc.jpg","../images/sf.jpg","../images/la.jpg","../images/austin.jpg","../images/sydney.jpg"];
-    // these are the images that are called upon to change the background, in order where .nyc is index '0'.
+    var images = ["url(images/nyc.jpg)", "url(images/sf.jpg)", "url(images/la.jpg)", "url(images/austin.jpg)", "url(images/sydney.jpg)"];
+    // these is my array of images that will appear in the background when the user types the acceptable text.
 
 
-    var input = [];
-    // this is where the text is stored when entered into #city-type.
+        // input.toLowerCase();
+        // now I need to figure out how to use the above thing
+        // so I can use it in each of the following 'if' statements.
+        // var newStuff = $("#city-type").val().toLowerCase();
 
 
-    function checkInput() {
-    // this is the function that activates the storage of the text that is entered.
-        var input = $("#city-type").val();
-                
-        if(input === parsedInput) {
-            changeImage()
+    function changeImages() {
+    // this is my function that is used below, that maps back to the image in my 
+        if (cityInputVal === "New York" || cityInputVal === "New York City" || cityInputVal === "NYC") {
+            $("body").css("background", images[0]);
+        } else if (cityInputVal === "San Francisco" || cityInputVal === "SF" || cityInputVal === "Bay Area" ) {
+            $("body").css("background", images[1]);
+        } else if (cityInputVal === "Los Angeles" || cityInputVal === "LA" || cityInputVal === "LAX") {
+            $("body").css("background", images[2]);
+        } else if (cityInputVal === "Austin" || cityInputVal === "ATX") {
+            $("body").css("background", images[3]);
+        } else if (cityInputVal === "Sydney" || cityInputVal === "SYD") {
+            $("body").css("background", images[4]);
         }
     }
 
 
-    function changeImage() {
-    // this is the function that changes the background when the inputs defined are entered.
-        var parsedInput = input.toLowerCase();
-        // this has created a variable that I can use in each of the following 'if' statements.
-        if(parsedInput === "New York" || parsedInput === "New York City" || parsedInput === "NYC") {
-            $("body").css("background",images[0])
-        }
-        else if(parsedInput === "San Francisco" || parsedInput === "SF" || parsedInput === "Bay Area") {
-            $("body").css("background",images[1])
-        }
-        else if(parsedInput === "Los Angeles" || parsedInput === "LA" || parsedInput === "LAX") {
-            $("body").css("background",images[2])
-        }
-        else if(parsedInput === "Austin" || parsedInput === "ATX") {
-            $("body").css("background",images[3])
-        }
-        else if(parsedInput === "Sydney" || parsedInput === "SYD") {
-            $("body").css("background",images[4])
-        }
-    }
-
-
-    $("#submit-btn").on("submit",function() {
-    // this is the function that works upon hitting 'submit'.
-    changeImage();
-    checkInput();
-    console.log("clicked submit");
+    $("#submit-btn").click(function(event) {
+    // this is the function that runs when the user enters acceptable text and presses 'enter' or clicks 'submit'.
+        cityInputVal = $("#city-type").val();
+        changeImages();
+        event.preventDefault();
     });
+
 
 });
